03ec50bf57184c514bb3793a3aaf40dba1c0bafc
habibmahaasin <habibirf23@gmail.com>
2025-09-14 17:46:58 +0700

    feat:initial setup shadcn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,12 @@
-import { login, signup } from './action';
+import { LoginForm } from '@/components/login-form';
+import { login } from './action';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor='email'>Email:</label>
-      <input id='email' name='email' type='email' required />
-      <label htmlFor='password'>Password:</label>
-      <input id='password' name='password' type='password' required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className='bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
+      <div className='w-full max-w-sm md:max-w-3xl'>
+        <LoginForm formAction={login} />
+      </div>
+    </div>
   );
 }
---
+++ src/components/login-form.tsx
@@ -0,0 +1,58 @@
+// components/login-form.tsx
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+interface LoginFormProps extends React.HTMLAttributes<HTMLDivElement> {
+  formAction?: (formData: FormData) => void | Promise<void>;
+}
+
+export function LoginForm({ className, formAction, ...props }: LoginFormProps) {
+  return (
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
+      <Card className='overflow-hidden p-0'>
+        <CardContent className='grid p-0 md:grid-cols-2 min-h-[80vh]'>
+          <div className='bg-muted relative hidden md:block' />
+          <form
+            className='p-6 md:p-8 flex flex-col justify-center'
+            action={formAction}
+          >
+            <div className='flex flex-col gap-6'>
+              <div className='flex flex-col items-center text-center'>
+                <h1 className='text-2xl font-bold'>Welcome back</h1>
+                <p className='text-muted-foreground text-balance'>
+                  Login to your Batumbuah Admin Account
+                </p>
+              </div>
+              <div className='grid gap-3'>
+                <Label htmlFor='email'>Email</Label>
+                <Input
+                  id='email'
+                  name='email'
+                  type='email'
+                  placeholder='admin@batumbuah.com'
+                  required
+                />
+              </div>
+              <div className='grid gap-3'>
+                <Label htmlFor='password'>Password</Label>
+                <Input
+                  id='password'
+                  name='password'
+                  type='password'
+                  placeholder='**********'
+                  required
+                />
+              </div>
+              <Button type='submit' className='w-full'>
+                Login
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
